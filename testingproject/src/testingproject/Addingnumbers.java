c308c76aa9b370939181b9c46a66ebf261da3961
radhika84 <radhika.rajesh06@gmail.com>
2016-12-08 23:13:32 -0500

    addition  of numbers

---
+++ testingproject/src/testingproject/Addingnumbers.java
@@ -0,0 +1,11 @@
+package testingproject;
+
+public class Addingnumbers {
+
+	public static void main(String[] args) {
+		int a=10,b=12;
+		System.out.println("sum of numbers is"+(a+b));
+		
+	}
+
+}
